4bf6c74b518df2a773a53c15698db24f76c1e23b
sungjine456 <sungjine456@gmail.com>
2023-09-27 18:36:14 +0900

    카메라 움직임 구현

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,234 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed = 10f;
+    [SerializeField] float _moveSmooth = 5f;
+    [SerializeField] float _zoomSmooth = 5f;
+
+    Camera _camera;
+    Controls _inputs;
+
+    bool _moving;
+    bool _zooming;
+
+    Vector3 _center;
+    float _right;
+    float _left;
+    float _up;
+    float _down;
+    float _angle;
+
+    float _zoom;
+    float _zoomMax;
+    float _zoomMin;
+    Vector2 _zoomPositionOnScreen;
+    Vector3 _zoomPositionInWorld;
+    float _zoomBaseValue;
+    float _zoomBaseDistance;
+
+    Transform _root;
+    Transform _pivot;
+    Transform _target;
+
+    void Awake()
+    {
+        _inputs = new Controls();
+        _root = new GameObject("CameraHelper").transform;
+        _pivot = new GameObject("CameraPivot").transform;
+        _target = new GameObject("CameraTarget").transform;
+    }
+
+    void OnEnable()
+    {
+        _inputs.Enable();
+        _inputs.Main.Move.started += _ => MoveStarted();
+        _inputs.Main.Move.canceled += _ => MoveCanceled();
+        _inputs.Main.TouchZoom.started += _ => ZoomStarted();
+        _inputs.Main.TouchZoom.canceled += _ => ZoomCanceled();
+    }
+
+    void OnDisable()
+    {
+        _inputs.Main.Move.started -= _ => MoveStarted();
+        _inputs.Main.Move.canceled -= _ => MoveCanceled();
+        _inputs.Main.TouchZoom.started -= _ => ZoomStarted();
+        _inputs.Main.TouchZoom.canceled -= _ => ZoomCanceled();
+        _inputs.Disable();
+    }
+
+    void MoveStarted()
+    {
+        _moving = true;
+    }
+
+    void MoveCanceled()
+    {
+        _moving = false;
+    }
+
+    void ZoomStarted()
+    {
+        Vector2 touch0 = _inputs.Main.TouchPosition0.ReadValue<Vector2>();
+        Vector2 touch1 = _inputs.Main.TouchPosition1.ReadValue<Vector2>();
+        _zoomPositionOnScreen = Vector2.Lerp(touch0, touch1, 0.5f);
+        _zoomPositionInWorld = CameraScreenPositionToPlanePosition(_zoomPositionOnScreen);
+        _zoomBaseValue = _zoom;
+
+        touch0.x /= Screen.width;
+        touch1.x /= Screen.width;
+        touch0.y /= Screen.height;
+        touch1.y /= Screen.height;
+
+        _zoomBaseDistance = Vector2.Distance(touch0, touch1);
+
+        _zooming = true;
+    }
+
+    void ZoomCanceled()
+    {
+        _zooming = false;
+    }
+
+    void Start()
+    {
+        _right = 10f;
+        _left = 10f;
+        _up = 10f;
+        _down = 10f;
+        _angle = 45f;
+
+        _zoom = 5f;
+        _zoomMin = 3;
+        _zoomMax = 10;
+
+        _pivot.SetParent(_root);
+        _target.SetParent(_pivot);
+
+        _root.position = _center;
+        _root.localEulerAngles = Vector3.zero;
+
+        _pivot.localPosition = Vector3.zero;
+        _pivot.localEulerAngles = new Vector3(_angle, 0f, 0f);
+
+        _target.localPosition = new Vector3(0f, 0f, -10f);
+        _target.localEulerAngles = Vector3.zero;
+
+        _camera = Camera.main;
+        _camera.transform.rotation = _target.rotation;
+        _camera.orthographicSize = _zoom;
+    }
+
+    void Update()
+    {
+        if (!Input.touchSupported)
+        {
+            float mouseScroll = _inputs.Main.MouseScroll.ReadValue<float>();
+
+            if (mouseScroll > 0)
+                _zoom -= 15f * Time.deltaTime;
+            else if (mouseScroll < 0)
+                _zoom += 15f * Time.deltaTime;
+        }
+
+        if (_zooming)
+        {
+            Vector2 touch0 = _inputs.Main.TouchPosition0.ReadValue<Vector2>();
+            Vector2 touch1 = _inputs.Main.TouchPosition1.ReadValue<Vector2>();
+
+            touch0.x /= Screen.width;
+            touch0.y /= Screen.height;
+            touch1.x /= Screen.width;
+            touch1.y /= Screen.height;
+
+            float currentDistance = Vector2.Distance(touch0, touch1);
+            _zoom = _zoomBaseValue - (currentDistance - _zoomBaseDistance) * _zoomSmooth;
+
+            Vector3 zoomCenter = CameraScreenPositionToPlanePosition(_zoomPositionOnScreen);
+            _root.position += _zoomPositionInWorld - zoomCenter;
+        }
+        else if (_moving)
+        {
+            Vector2 move = _inputs.Main.MoveDelta.ReadValue<Vector2>();
+
+            if (move != Vector2.zero)
+            {
+                move.x /= Screen.width;
+                move.y /= Screen.height;
+                _root.position -= move.x * _moveSpeed * _root.right;
+                _root.position -= move.y * _moveSpeed * _root.forward;
+            }
+        }
+
+        AdjustBounds();
+
+        if (_camera.orthographicSize != _zoom)
+            _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, _zoom, _zoomSmooth * Time.deltaTime);
+        if (_camera.transform.position != _target.position)
+            _camera.transform.position = Vector3.Lerp(_camera.transform.position, _target.position, _moveSmooth * Time.deltaTime);
+    }
+
+    void AdjustBounds()
+    {
+        if (_zoom < _zoomMin) _zoom = _zoomMin;
+        if (_zoom > _zoomMax) _zoom = _zoomMax;
+
+        float h = PlaneOrtographicSize();
+        float w = h * _camera.aspect;
+
+        if (h > (_up + _down) / 2f)
+        {
+            float v = (_up + _down) / 2f;
+            _zoom = v * Mathf.Sin(_angle * Mathf.Deg2Rad);
+        }
+
+        if (w > (_right + _left) / 2f)
+        {
+            float v = (_right + _left) / 2f / _camera.aspect;
+            _zoom = v * Mathf.Sin(_angle * Mathf.Deg2Rad);
+        }
+
+        h = PlaneOrtographicSize();
+        w = h * _camera.aspect;
+
+        Vector3 tr = _root.position + _root.right * w + _root.forward * h;
+        Vector3 tl = _root.position - _root.right * w + _root.forward * h;
+        Vector3 dl = _root.position - _root.right * w - _root.forward * h;
+
+        if (tr.x > _center.x + _right)
+            _root.position += Vector3.left * Mathf.Abs(tr.x - (_center.x + _right));
+        else if (tl.x < _center.x - _left)
+            _root.position += Vector3.right * Mathf.Abs(_center.x - _left - tl.x);
+        else if (tr.z > _center.z + _up)
+            _root.position += Vector3.back * Mathf.Abs(tr.z - (_center.z + _up));
+        else if (dl.z < _center.z - _down)
+            _root.position += Vector3.forward * Mathf.Abs(_center.z - _down - dl.z);
+    }
+
+    float PlaneOrtographicSize()
+    {
+        float h = _zoom * 2f;
+
+        return h / Mathf.Sin(_angle * Mathf.Deg2Rad) / 2f;
+    }
+
+    Vector3 CameraScreenPositionToWorldPosition(Vector2 position)
+    {
+        float h = _camera.orthographicSize * 2f;
+        float w = _camera.aspect * h;
+
+        Vector3 ancher = _camera.transform.position - (_camera.transform.right * w / 2f) - (_camera.transform.up * h / 2f);
+
+        return ancher + (_camera.transform.right * position.x / Screen.width * w) + (_camera.transform.up * position.y / Screen.height * h);
+    }
+
+    Vector3 CameraScreenPositionToPlanePosition(Vector2 position)
+    {
+        Vector3 point = CameraScreenPositionToWorldPosition(position);
+
+        float h = point.y - _root.position.y;
+        float x = h / Mathf.Sin(_angle * Mathf.Deg2Rad);
+
+        return point + _camera.transform.forward * x;
+    }
+}
